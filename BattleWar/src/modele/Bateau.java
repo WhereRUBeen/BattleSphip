51135233f4b628d31a732d6afa2d257136ddced3
WhereRUBeen <brice.mimifir.a@gmail.com>
2017-09-20 22:51:50 -0400

    add methodes

--- BattleWar/src/modele/Bateau.java
+++ BattleWar/src/modele/Bateau.java
@@ -8,7 +8,17 @@ package modele;
  *
  */
 public class Bateau {
+	
+	private String nom;
+	private int taille;
+	private int nFoisToucher;
+	private int tabBateau[];
+	private int x,y;
 
+	public Bateau(String name,int grandeur) {
+		this.nom = name;
+		
+	}
 	/**
 	 * @param args
 	 */
