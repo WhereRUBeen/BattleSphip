e44eebdaca54af8298a011adefc14e92cf8a40df
WhereRUBeen <brice.mimifir.a@gmail.com>
2017-09-21 00:39:15 -0400

    Add five class extends from class bateau

---
+++ BattleWar/src/modele/Carrier.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package modele;
+
+/**
+ * @author Been WhereU
+ *
+ */
+public class Carrier extends Bateau {
+
+	//Bateau de taille 5 
+	public Carrier(String name, int grandeur) {
+		super(name, 5);
+	}
+
+}
---
+++ BattleWar/src/modele/Destroyer.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package modele;
+
+/**
+ * @author Been WhereU
+ *
+ */
+public class Destroyer extends Bateau {
+
+	public Destroyer(String name, int grandeur) {
+		super(name, 2);
+	}
+
+}
