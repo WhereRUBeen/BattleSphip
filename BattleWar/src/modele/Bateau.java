0d03ddfe33c0ddc7e6d37b5fd38b38f74bd0e7d3
Been WhereU <Been WhereU@Dyvaa>
2017-09-20 22:05:34 -0400

    class Bateau

---
+++ BattleWar/src/modele/Bateau.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package modele;
+
+/**
+ * @author Been WhereU
+ *
+ */
+public class Bateau {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
